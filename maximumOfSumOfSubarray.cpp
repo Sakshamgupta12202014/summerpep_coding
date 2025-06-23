36a8f8558c85c814d5a84ee4437c099a49781e34
Sakshamgupta12202014 <gupta.saksham0401@gmail.com>
2025-06-23 10:46:59 +0530

    23/6/2025 2

--- maximumOfSumOfSubarray.cpp
+++ maximumOfSumOfSubarray.cpp
@@ -10,6 +10,7 @@ void maximumOfSumOfSubarray(vector<int> arr, int k) {
     int i=0,j=0;
     int sum = 0;
     int size = 0;
+    int m, n;
 
     while(j < arrSize) {
 
@@ -22,17 +23,25 @@ void maximumOfSumOfSubarray(vector<int> arr, int k) {
 
         
         if(sum < k) {
+            if((j-i+1) > max_size){     // yaar tabhi subarray store karo jab tumhe pahle se bada size mila hai logial hai bhai  initially max_size is INT_MIN so 1 bhi bada hoga toh uss subarray ke indices store krega then max_size badega aur agar pahle se bada  hoga tabhi hum subarray ke indices store karenge 
+                m = i;
+                n = j;
+            }
             max_size = max(max_size, j-i+1);
 
-            for(int a=i; a<=j; a++){
-                cout << arr[a] << "  ";
-            }
-            cout << endl;
+            // for(int a=i; a<=j; a++){
+            //     cout << arr[a] << "  ";
+            // }
+            // cout << endl;
         }
         j++;
     }
 
     cout << "Max subarray size: " << max_size << endl;
+    for(int l=m; l<=n; l++) {
+        cout << arr[l] << "  ";
+    }
+    cout << endl;
 }
 
 int main() {
